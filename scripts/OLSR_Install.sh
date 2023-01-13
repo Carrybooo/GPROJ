92baec10d5c27228d35209c8a00a2f633d787a02
WalfroyB <118510789+WalfroyB@users.noreply.github.com>
2023-01-13 15:58:33 +0100

    Update OLSR_Install.sh

--- scripts/OLSR_Install.sh
+++ scripts/OLSR_Install.sh
@@ -1,15 +1,45 @@
 # Terminal à la racine
+# sudo rm -rf OLSR
 # sudo su
 # cd /etc/apt
 # nano sources.list
 # mettre un #ligne 3
-# sudo bash OLSR_Install.sh
+
+# En user et pas en root !
+# chmod +x OLSR_Install.sh
+# ./OLSR_Install.sh
+
 sudo apt update -y && sudo apt upgrade -y &&\
 sudo apt install -y git cmake build-essential libnl-3* gcc libtomcrypt-dev &&\
 cd /home/debian/ &&\
+if [ -d "/home/debian/OLSR" ]; then
+	sudo rm -rfv /home/debian/OLSR
+fi
 sudo mkdir -p -m 777 OLSR &&\
 sudo git clone https://github.com/OLSR/OONF OLSR &&\
 cd OLSR/build &&\
 sudo cmake .. &&\
 sudo make &&\
+
+sudo mkdir -p /etc/olsrd2 &&\
+sudo ln -sfv /home/debian/OLSR/apps/olsrd2/debian/olsrd2.conf /etc/olsrd2/ &&\
+sudo ln -sfv /home/debian/OLSR/apps/olsrd2/debian/olsrd2.service /etc/systemd/system/ &&\
+sudo mkdir -p /home/debian/OLSR/run
+sudo ln -sfv /home/debian/OLSR/apps/olsrd2/debian/olsrd2.conf /home/debian/OLSR/run/ &&\
+sudo ln -sfv /home/debian/OLSR/apps/olsrd2/debian/olsrd2.service /home/debian/OLSR/run/ &&\
+sudo ln -sfv /home/debian/OLSR/build/olsrd2_static /usr/sbin/ &&\
+sudo ln -sfv /home/debian/OLSR/build/olsrd2_dynamic /usr/sbin/ &&\
+sudo ln -sfv /home/debian/OLSR/build/olsrd2_dlep_static /usr/sbin/ &&\
+sudo ln -sfv /home/debian/OLSR/build/olsrd2_dlep_dynamic /usr/sbin/ &&\
+sudo systemctl daemon-reload &&\
+
 echo Installation réussie
+
+# Pour lancer olsrd2
+# sudo systemctl start olsrd2.service
+
+# Pour vérifier olsrd2 actif
+# sudo systemctl stats olsrd2.service
+
+# Pour stopper olsrd2
+# sudo systemctl stop olsrd2.service
