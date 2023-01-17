e976d08cffb493357b8c9335562ba4fc19d75878
WalfroyB <118510789+WalfroyB@users.noreply.github.com>
2023-01-17 17:08:24 +0100

    Update sender.rs

--- netperf/src/sender.rs
+++ netperf/src/sender.rs
@@ -24,14 +24,14 @@ fn main() {
         1 => config.ip1.parse().unwrap(),
         2 => config.ip2.parse().unwrap(),
         3 => config.ip3.parse().unwrap(),
-        4 => config.ip3.parse().unwrap(),
+        4 => config.ip4.parse().unwrap(),
         _ => {println!("Config Error :\nUnrecognized PC number :\"{}\", unable to continue.", config.num_dist); exit(1)},
     };
     let local_addr: Ipv4Addr = match config.num_local{
         1 => config.ip1.parse().unwrap(),
         2 => config.ip2.parse().unwrap(),
         3 => config.ip3.parse().unwrap(),
-        4 => config.ip3.parse().unwrap(),
+        4 => config.ip4.parse().unwrap(),
         _ => {println!("Config Error :\nUnrecognized PC number :\"{}\", unable to continue.", config.num_local); exit(1)},
     };
 
